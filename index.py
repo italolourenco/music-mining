138c9330577a92f4196a0a6c2bf8057f6c065d23
italolourenco <italolt10@gmail.com>
2020-08-14 00:13:28 -0300

    feat: pre processor music data

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import nltk
+import re
+
 
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+
+
+# Pré-Processamento dos dados (OK)
+# Indexação
 
 
 songs60DataSet = "data/musicReport-60.csv"
@@ -12,10 +20,59 @@ songs2000DataSet = "data/musicReport-2000.csv"
 songs2010DataSet = "data/musicReport-2010.csv"
 
 
+def musicToken(text):
+    tokens = word_tokenize(text.values[0])
+    return tokens
+
+def musicRemoveStopWords(text):
+
+    stopWords = set(stopwords.words('portuguese'))
+    wordsFiltered = []
+
+    for w in text:
+        if w not in stopWords:
+            wordsFiltered.append(w)
+
+    return wordsFiltered
+
+def musicRemoveNumbers(text):
+    regex = re.compile('[-|0-9]')
+    filtered = [i for i in text if not regex.match(i)]
+    return filtered
+
+def musicRemoveSpecialCharacter(text):
+
+    regex = re.compile(r'[-./?!,":;()\']')
+    filtered = [i for i in text if not regex.match(i)]
+    return filtered
+
+def filterMusicText(text):
+
+    textWithoutStopWords = musicRemoveStopWords(text)
+
+    textWithoutNumbers = musicRemoveNumbers(textWithoutStopWords)
+
+    textWithoutSpecialCharacter = musicRemoveSpecialCharacter(textWithoutNumbers)
+
+    return textWithoutSpecialCharacter
+
+
+
+
 def main():
 
+    music_data = {}
+
     df_60songs = pd.read_csv(songs60DataSet, sep=';')
-    print(df_60songs)
+    music_names = list(df_60songs['Music'])
+
+    for name in music_names:
+        music_data = df_60songs[df_60songs['Music'] == name]
+        music_token = musicToken(music_data['Text'])
+        filtered_tokens = filterMusicText(music_token)
+        print(filtered_tokens)
+
+        
 
 
 
