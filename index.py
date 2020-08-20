40374c6ea091c3b7c942ff22c4a8786ad1694bd7
italolourenco <italolt10@gmail.com>
2020-08-19 23:00:55 -0300

    feat: add musicReports list

--- index.py
+++ index.py
@@ -1,23 +1,15 @@
 import pandas as pd
 import nltk
 import re
-
-
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
-
-
-# Pré-Processamento dos dados (OK)
-# Indexação
-
-
-songs60DataSet = "data/musicReport-60.csv"
-songs70DataSet = "data/musicReport-70.csv"
-songs80DataSet = "data/musicReport-80.csv"
-songs90DataSet = "data/musicReport-90.csv"
-songs2000DataSet = "data/musicReport-2000.csv"
-songs2010DataSet = "data/musicReport-2010.csv"
+s60DataSet = "data/musicReport-60.csv"
+s70DataSet = "data/musicReport-70.csv"
+s80DataSet = "data/musicReport-80.csv"
+s90DataSet = "data/musicReport-90.csv"
+s2000DataSet = "data/musicReport-2000.csv"
+s2010DataSet = "data/musicReport-2010.csv"
 
 
 def musicToken(text):
@@ -61,14 +53,14 @@ def filterMusicText(text):
 
 def main():
 
+    musicReports = [s60DataSet, s70DataSet, s80DataSet, s90DataSet, s2000DataSet, s2010DataSet]
+
     music_data = {}
 
     df_60songs = pd.read_csv(songs60DataSet, sep=';')
     music_names = list(df_60songs['Music'])
 
     for name in music_names:
-        print(name)
-        print('\n')
         music_data = df_60songs[df_60songs['Music'] == name]
         music_token = musicToken(music_data['Text'])
         filtered_tokens = filterMusicText(music_token)
